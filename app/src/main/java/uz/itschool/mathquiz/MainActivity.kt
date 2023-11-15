bb9f2155473faf5f6f3fb752274a1a5cda40724c
User_3 <rmuhammadqodir58@gmail.com>
2023-11-15 14:55:00 +0500

    second commit

--- app/src/main/java/uz/itschool/mathquiz/MainActivity.kt
+++ app/src/main/java/uz/itschool/mathquiz/MainActivity.kt
@@ -7,14 +7,20 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import uz.itschool.mathquiz.ui.theme.MathQuizTheme
 
 class MainActivity : ComponentActivity() {
@@ -38,10 +44,13 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun GreetingPreview() {
     MathQuizTheme{
-        Box(modifier = Modifier.fillMaxSize(), colors = ButtonDefaults.buttonColors(backgroundColor = Color.Blue)){
+        Box(modifier = Modifier.fillMaxSize().padding(10.dp), contentAlignment = Alignment.Center){
         Column {
-            Button(onClick = {}) {
+            Button(onClick = {}, modifier = Modifier.width(200.dp).height(45.dp)) {
                 Text(text = "New Game")
+            }
+            Button(onClick = {}, modifier = Modifier.width(200.dp).height(45.dp)) {
+                Text(text = "Records")
             }
         }
     }
